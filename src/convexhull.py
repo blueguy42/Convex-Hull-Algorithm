8abb702961698bd2c5a9b0d8368fd451ee93bb95
Ahmad Alfani Handoyo <ahmadalfanihandoyo@hotmail.com>
2022-02-24 22:53:32 +0700

    documentating and modularizing algorithm

--- src/convexhull.py
+++ src/convexhull.py
@@ -1,55 +1,4 @@
-import numpy as np
-
-def kirikanan_garis(arr, p1, pn, px):
-    '''Memeriksa apakah sebuah titik berada di sebelah kiri atau
-    kanan garis menggunakan penentuan determinan. Bila sebelah
-    kiri maka hasil positif, kanan negatif, di garis 0.'''
-
-    x1 = arr[p1][0]; y1 = arr[p1][1]; x2 = arr[pn][0]; y2 = arr[pn][1]; x3 = arr[px][0]; y3 = arr[px][1]
-
-    return x1*y2 + x3*y1 + x2*y3 - x3*y2 - x2*y1 - x1*y3
-
-
-def sudut(a, b, c):
-    '''Mengembalikan sudut dari segitiga ABC dengan B sudut yang
-    terapit.'''
-
-    # cos theta = ba.bc / |ba|*|bc|
-    ba = a - b; bc = c - b
-    cosine = np.dot(ba, bc)/(np.linalg.norm(ba)*np.linalg.norm(bc))
-
-    return np.degrees(np.arccos(cosine))
-
-
-def minmax_x(arr_np):
-    """Mengembalikan tuple indeks titik dengan nilai x minimal
-    dan maksimal."""
-
-    # Mencari nilai x minimal atau maksimal
-    x_coordinates = []
-    for i in range(len(arr_np)):
-        x_coordinates.append(arr_np[i][0])
-    x_min = min(x_coordinates)
-    x_max = max(x_coordinates)
-
-    # Menentukan indeks dengan x minimal
-    min_index = 0
-    found = True
-    while min_index < len(arr_np) and found:
-        if arr_np[min_index][0] == x_min:
-            found = False
-        else: min_index += 1
-        
-    # Menentukan indeks dengan x maksimal
-    max_index = 0
-    found = True
-    while (max_index < len(arr_np) and found):
-        if arr_np[max_index][0] == x_max:
-            found = False
-        else: max_index += 1
-    
-    return (min_index, max_index)
-
+from geometry import *
 
 def convexhull(arr):
     """Mengembalikan hasil pasang titik garis pembentuk bidang
@@ -75,6 +24,7 @@ def convexhull(arr):
     # Masuk fungsi rekursi convex hull kanan garis
     kanan = convexhull_recursive(arr_used, right_arr, pn, p1)
     return kiri + kanan
+
 
 def convexhull_recursive(arr_used, arr, p1, pn):
     """Fungsi rekursif penentu pasangan indeks titik garis
